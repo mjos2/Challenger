05ce8153543f17765017beefce577d88a178bffe
Marconi Souza <mjos2@discente.ifpe.edu.br>
2023-05-04 11:25:01 -0300

    Changeler

---
+++ cjs/index.js
@@ -0,0 +1,27 @@
+"use strict";
+var descriptografada = document.querySelector("#input");
+descriptografada.focus();
+var text_Descript = descriptografada.value;
+var button1 = document.querySelector("#button1");
+
+function criptografar(){ 
+  for(let indice=0; indice<descriptografada.lenght; indice++);
+    if(con)   
+}  
+
+button1.onclick = criptografar;
+
+  //  for (i=0; i < text_Descript.length; i++){
+  //       if(text_Descript[i] = "a"){
+  //          criptografar.push()
+  //       }
+  //  }
+//}
+
+
+//A letra "e" é convertida para "enter"
+//A letra "i" é convertida para "imes"
+//A letra "a" é convertida para "ai"
+//A letra "o" é convertida para "ober"
+//A letra "u" é convertida para "ufat"
+
